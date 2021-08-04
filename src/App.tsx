85ca22295bb83948ba52acc963b2b001dc6544a1
Roberto Salazar <robertorenesc@gmail.com>
2021-08-04 13:55:52 -0600

    RInitial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
 import React from 'react'
-import {
-    BrowserRouter, Route, Switch
-  } from "react-router-dom";
-import HomeWorld from './components/HomeWorld';
-import StarWars from './components/StarWars'
 
 const App: React.FC = () => {
     return <>
-        <BrowserRouter>
-            <Switch>
-                <Route exact path="/" component={StarWars}></Route>
-                <Route path="/homeworld/:id" component={HomeWorld}></Route>
-            </Switch>
-        </BrowserRouter>
+        <h2>Testing...</h2>
     </>
 
 }
