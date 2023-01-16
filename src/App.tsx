cf13c841d80fcf39850e811230e76a17ddb4f48a
Roberto Salazar <rtsz@gft.com>
2023-01-16 17:43:51 -0600

    Added solution with components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { CharacterTable } from './Home'
 import { Character, CharacterResponse } from './model/Character'
 import starWarsService from './services/StarWarsService'
 
@@ -15,56 +16,7 @@ const App: React.FC = () => {
 
     return <>
         <h2>List of Characters</h2>
-        <table>
-            <thead>
-                <tr>
-                    <td>
-                        Name
-                    </td>
-                    <td>
-                        Height
-                    </td>
-                    <td>
-                        Hair Color
-                    </td>
-                    <td>
-                        Skin Color
-                    </td>
-                    <td>
-                        Eye Color
-                    </td>
-                    <td>
-                        
-                    </td>
-                </tr>
-            </thead>
-            <tbody>
-                { characters.map((c: Character) => {
-                    return <>
-                        <tr>
-                            <td>
-                                { c.name }
-                            </td>
-                            <td>
-                                { c.height }
-                            </td>
-                            <td>
-                                { c.hair_color }
-                            </td>
-                            <td>
-                                { c.skin_color }
-                            </td>
-                            <td>
-                                { c.eye_color }
-                            </td>
-                            <td>
-                                <a href={c.homeworld}>Go to Homeworkd</a>
-                            </td>
-                        </tr>
-                    </>         
-                })}
-            </tbody>
-        </table>
+        <CharacterTable characters={characters} />
     </>
 
 }
